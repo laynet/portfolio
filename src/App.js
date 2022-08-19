52d79e28bd26e788ba439e23152a85ad517548a4
Layne <layneingramtaylor@gmail.com>
2022-08-19 10:16:24 -0500

    added web link screen shots

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import headshot from './assets/laynet.jpeg';
+import projectData from './project-data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>home</li>
+          <li>web</li>
+          <li>experience</li>
+          <li>resume</li>
+          <li>contact</li>
+        </ul>
+      </nav>
+      <section className='bio'>
+        <img className='headshot' src={headshot} alt='' />
+        <div className='bio-content'>
+          <h1>Layne Taylor</h1>
+          <h2>Front end web developer for hire</h2>
+          <p>
+            Motivated junior web developer looking for a role as a front end or
+            full stack developer. 4 years of coding experience with a varied and
+            diverse career and education.
+          </p>
+        </div>
+      </section>
+      <section className='projects card-grid'>
+        {projectData.map((item) => {
+          return (
+            <div className='card'>
+              <a href={item.link} target='_blank'>
+                <img src={item.screenShot} alt='' />
+              </a>
+            </div>
+          );
+        })}
+      </section>
+    </>
   );
 }
 
---
+++ src/project-data.js
@@ -0,0 +1,128 @@
+import digBizCard from './assets/digital-biz-card.png';
+import travelJournal from './assets/travel-journal.png';
+import orderSum from './assets/order-summary.png';
+import memeGenerator from './assets/meme-generator.png';
+import markdownApp from './assets/markdown-notes.png';
+import tenzies from './assets/tenzies-game.png';
+import spaceTourism from './assets/space-tourism.png';
+import statsPreview from './assets/stats-preview-card.png';
+import adviceGenerator from './assets/advice-generator.png';
+import elearning from './assets/elearning-landing-page.png';
+import productPreview from './assets/product-preview-card.png';
+import interactiveRating from './assets/interactive-rating.png';
+import meetLandingPage from './assets/meet-landing-page/meet-landing-desktopss.png';
+import threeColPrev from './assets/3-column-preview-card/3-col-mobiless.png';
+const projectData = [
+  {
+    name: 'Digital Business Card',
+    link: 'https://delightful-meerkat-d44884.netlify.app/',
+    screenShot: 'https://i.imgur.com/8FOVMaU.png',
+  },
+  {
+    name: 'My Travel Journal',
+    link: 'https://lively-ganache-a6ff36.netlify.app/',
+    screenShot: 'https://i.imgur.com/6D3QUPj.png',
+  },
+  {
+    name: 'Order Summary',
+    link: 'https://precious-rugelach-687791.netlify.app/',
+    screenShot: 'https://i.imgur.com/iFoCmWf.png',
+  },
+  {
+    name: 'Meme Generator',
+    link: 'https://incandescent-nougat-7cb478.netlify.app/',
+    screenShot: 'https://i.imgur.com/a8tThjK.png',
+  },
+  {
+    name: 'Markdown Notes App',
+    link: 'https://zingy-capybara-34fb14.netlify.app/',
+    screenShot: 'https://i.imgur.com/d910n6r.png',
+  },
+  {
+    name: 'Tenzies Game App',
+    link: 'https://animated-taffy-960ebc.netlify.app/',
+    screenShot: 'https://i.imgur.com/VMlZ4aY.png',
+  },
+  {
+    name: 'Space Tourism',
+    link: 'https://tourmaline-biscuit-d23dd3.netlify.app/',
+    screenShot: 'https://i.imgur.com/nhAXpNB.png',
+  },
+  {
+    name: 'Stats Preview Card',
+    link: 'https://ephemeral-medovik-fd0ce3.netlify.app/',
+    screenShot: 'https://i.imgur.com/lTLj1QB.png',
+  },
+  {
+    name: 'Advice Generator',
+    link: 'https://sage-clafoutis-f8bb6c.netlify.app/',
+    screenShot: 'https://i.imgur.com/Sp52Y5W.png',
+  },
+  {
+    name: 'Skilled E-Learning Landing Page',
+    link: 'https://zesty-sable-ec4185.netlify.app/',
+    screenShot: 'https://i.imgur.com/P2M4kom.png',
+  },
+  {
+    name: 'Product Preview Card',
+    link: 'https://zesty-sable-ec4185.netlify.app/',
+    screenShot: 'https://i.imgur.com/sVEwfq7.png',
+  },
+  {
+    name: 'Interactive Rating Component',
+    link: 'https://astounding-mousse-0d8d81.netlify.app/',
+    screenShot: 'https://i.imgur.com/yfhwQaP.png',
+  },
+  {
+    name: 'Meet Landing Page',
+    link: 'https://transcendent-queijadas-95f514.netlify.app/',
+    screenShot: 'https://i.imgur.com/dpTbYSv.png',
+  },
+  {
+    name: '3 Column Preview Card',
+    link: 'https://transcendent-queijadas-95f514.netlify.app/',
+    screenShot: 'https://i.imgur.com/heuLfyB.png',
+  },
+  {
+    name: 'Birthday reminder',
+    link: 'https://fascinating-piroshki-40ca46.netlify.app/',
+    screenShot: 'https://i.imgur.com/8SWyzpi.png',
+  },
+  {
+    name: 'Tour app',
+    link: 'https://shiny-klepon-fd05d1.netlify.app/',
+    screenShot: 'https://i.imgur.com/amQ9Bmo.png',
+  },
+  {
+    name: 'Review App',
+    link: 'https://eclectic-dolphin-f504ee.netlify.app/',
+    screenShot: 'https://i.imgur.com/lq5ou28.png',
+  },
+  {
+    name: 'Accordion',
+    link: 'https://gilded-pie-457b45.netlify.app/',
+    screenShot: 'https://i.imgur.com/nerUxkG.png',
+  },
+  {
+    name: 'Menu app',
+    link: 'https://tangerine-travesseiro-136ea1.netlify.app/',
+    screenShot: 'https://i.imgur.com/IKEK08e.png',
+  },
+  {
+    name: 'Tabs app',
+    link: 'https://earnest-torrone-ef91ae.netlify.app/',
+    screenShot: 'https://i.imgur.com/BHxdoIQ.png',
+  },
+  {
+    name: 'Slider app',
+    link: 'https://tubular-frangipane-16f1c6.netlify.app/',
+    screenShot: 'https://i.imgur.com/6V50i6G.png',
+  },
+  {
+    name: 'Lorem ipsum generator',
+    link: 'https://visionary-liger-79bd9f.netlify.app/',
+    screenShot: 'https://i.imgur.com/ZNal8uz.png',
+  },
+];
+
+export default projectData;
